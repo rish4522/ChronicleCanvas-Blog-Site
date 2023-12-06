f8c00e9c795703dff76f89c27f41b896561a00e5
RishEverec <117337128+rish4522@users.noreply.github.com>
2023-12-06 11:49:46 +0530

    Second Commit

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 import express from 'express';
 
-
 const app = express();
 const port = 3000;
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.send("<h1>Chronicle Canvas</h1>");
+    res.render("index.ejs");
+});
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+app.get("/category", (req, res) => {
+    res.render("categories.ejs");
+});
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+});
+app.get("/bio", (req, res) => {
+    res.render("bio.ejs");
+});
+app.get("/faq", (req, res) => {
+    res.render("faq.ejs");
 });
 
 app.listen(port, () => {
